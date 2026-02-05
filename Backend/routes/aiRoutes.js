3076deba10ca3c8e07f6ceb975d8fb9bc19a2054
Naveen Kumar <kumarnaveen897941@gmail.com>
2026-02-05 17:08:11 +0530

    add the routes

--- Backend/routes/aiRoutes.js
+++ Backend/routes/aiRoutes.js
@@ -1,6 +1,11 @@
 import express from "express";
 import {
-
+generateFlashcards,
+generateQuiz,
+generateSummary,
+chat,
+explainConcept,
+getChatHistory
 }from "../Controllers/aiController.js"
 import protect from "../middleware/auth.js";
-const 
+const router=express.Router();
