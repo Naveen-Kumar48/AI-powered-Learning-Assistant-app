42dc1963d1830031be0869fddd25c48193a7d528
Naveen Kumar <kumarnaveen897941@gmail.com>
2026-02-05 17:18:48 +0530

    designing the ai routes

--- Backend/routes/aiRoutes.js
+++ Backend/routes/aiRoutes.js
@@ -9,3 +9,6 @@ getChatHistory
 }from "../Controllers/aiController.js"
 import protect from "../middleware/auth.js";
 const router=express.Router();
+router.use(protect);
+router.post("/generate-flashcard",generateFlashcards);
+router.post("/generate-quiz",generateQuiz);
