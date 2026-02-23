c6a04343719f7a25ec47cf76dca606ad245294f5
Naveen Kumar <kumarnaveen897941@gmail.com>
2026-02-23 12:36:48 +0530

    feat: Add progress routes and controller for dashboard functionality

---
+++ Backend/routes/progressRoutes.js
@@ -0,0 +1,17 @@
+import express from "express"; 
+import {
+    getDashboard,
+} from '../Controllers/progressController.js'
+import protect from '../middleware/auth.js'
+const router = express.Router();
+router.use(protect)
+
+router.get('/dashboard',getDashboard)
+
+
+
+
+
+
+
+export default router;
