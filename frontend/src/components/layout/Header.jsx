cfbecd26374a11bf8b084d1464cee5d8ae529a31
Naveen Kumar <kumarnaveen897941@gmail.com>
2026-02-26 22:33:43 +0530

    feat: enhance Header and Sidebar components with improved user interface and navigation functionality

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import { useAuth } from "../../components/context/AuthContext"
-import { useNavigate } from 'react-router-dom'
+
 import { Bell, User, Menu } from "lucide-react"
 
 const Header = ({ toggleSidebar }) => {
   const { user } = useAuth()
-  const navigate = useNavigate()
   return <header className="sticky top-0 z-40 w-full h-16 bg-white/95 backdrop-blur-md border-b border-neutral-200 px-6 py-4 flex items-center justify-between">
     <div className="flex items-center  justify-between h-full px-8">
       {/* Mobile Menu  Button */}
@@ -18,18 +17,23 @@ const Header = ({ toggleSidebar }) => {
       </div>
     </div>
     <div className="flex items-center gap-3">
-      <button className="reletive inline-flex items-center justify-center w-10 h-10 text-slate-600 hover:bg-slate-900 hover-bg-slate-100 rounded-xl transition-all duration-200">
-        <Bell size={20} strokeWidth={2} className='group-hover:scale-110 transition-transform duration-200' />
-        <span className='absolute top-1.5 right-1.5 w-2 h-2 bg-emerald-500 rounded-full ring-2 ring-white'></span>
+      <button className="inline-flex items-center justify-center w-10 h-10 text-slate-600 hover:text-slate-900 hover:bg-slate-100 rounded-xl transition-all duration-200 group">
+        <div className="relative">
+          <Bell size={22} strokeWidth={2} className='group-hover:scale-110 transition-transform duration-200' />
+          <span className='absolute top-0 right-0 w-2.5 h-2.5 bg-emerald-500 rounded-full border-2 border-white translate-x-0.5 -translate-y-0.5'></span>
+        </div>
       </button>
 
       {/* userProfile */}
       <div className="flex items-center gap-3 pl-3 border-l border-slate-200/60">
         <div className="flex items-center  gap-3 px-3  py-1.5 rounded-xl hover:bg-slate-50 transition-colors duration-200 cursor-pointer group" >
-          <div className='w-9 h-9 rounded-xl bg-gradient-to-br from-emerald-400 to-teal-500 flex items-center justify-center text-white shadow-md shadow-emerald-500/20 group-hover'>
+          <div className='w-9 h-9 rounded-xl bg-gradient-to-br from-emerald-400 to-teal-500 flex items-center justify-center text-white shadow-md shadow-emerald-500/20 group-hover:shadow-lg group-hover:shadow-emerald-500/30 transition-all duration-200'>
             <User size={18} strokeWidth={2.5} className='group-hover:text-slate-900 transition-colors duration-200' />
           </div>
-          <p className="text-neutral-700">{user?.email || 'user@example.com'}</p>
+          <div className=''>
+            <p className="text-sm font-semibold text-slate-900">{user?.username || 'Username'}</p>
+            <p className='text-xs font-medium text-slate-500'>{user?.email || 'user@gmail.com'}</p>
+          </div>
         </div>
       </div>
     </div>
--- frontend/src/components/layout/Sidebar.jsx
+++ frontend/src/components/layout/Sidebar.jsx
@@ -1,8 +1,91 @@
 import React from 'react'
+import { NavLink, useNavigate } from 'react-router-dom'
+import { useAuth } from "../../components/context/AuthContext"
+import { LayoutDashboard, FileText, User, BrainCircuit, BookOpen, X, LogOut } from 'lucide-react'
+const Sidebar = ({ isSidebarOpen, toggleSidebar }) => {
+  const { logout } = useAuth()
+  const navigate = useNavigate()
+  const handleLogout = () => {
+    logout()
+    navigate('/login')
+  }
 
-const Sidebar = () => {
+  const navLinks = [
+    {
+      to: "/dashboard",
+      icon: LayoutDashboard,
+      text: "Dashboard"
+    },
+    {
+      to: "/documents",
+      icon: FileText,
+      text: "Documents"
+    },
+    {
+      to: "/flashcards",
+      icon: BookOpen,
+      text: "Flashcards"
+    },
+    {
+      to: "/profile",
+      icon: User,
+      text: "Profile"
+    }
+  ]
+
   return (
-    <div>Sidebar</div>
+    <>
+      <div className={`fixed inset-0  bg-black/30 z-40 md:hidden transition-opacity duration-300 ${isSidebarOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}`} onClick={toggleSidebar}
+        aria-hidden="true"></div>
+
+      <aside className={`fixed top-0 left-0 h-full w-64 bg-white/90 backdrop-blur-lg border-r border-slate-200/60 z-50 md:relative md:relative md:w-64 md:shrink-0 md:flex md:flex-col md:translate-x-0  transition-transform duration-300 ease-in-out  ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'}`}>
+        {/* Logoot and Close Button  for Mobile*/}
+        <div className=' flex item-center justify-between h-16  px-5'>
+          <div>
+            <div className='flex items-center gap-2'>
+              <BrainCircuit size={20} strokeWidth={2.5} className='text-slate-900' />
+            </div>
+            <h1 className='text-sm font-bold text-slate-500'>AI Learning Assistant</h1>
+            <button onClick={toggleSidebar} className='md:hidden inline-flex items-center justify-center w-10 h-10 text-slate-900 hover:bg-slate-100 rounded-xl transition-all duration-200'>
+              <X size={24} />
+            </button>
+          </div>
+          {/* Navigation */}
+          <nav className='flex flex-col gap-2 mt-8'>
+            {navLinks.map((link) => (
+              <NavLink
+                key={link.to}
+                to={link.to}
+                onClick={toggleSidebar}
+                className={({ isActive }) => `group flex items-center gap-3 px-4 py-2.5 text-sm font-semibold rounded-xl transition-all duration-200 ${isActive ? 'bg-linear-to-r from-emerald-500 to-teal-700 hover:bg-slate-100 hover:text-slate-900'}`}
+              >
+                {({ isActive }) => (
+                  <>
+                    <link.icon
+                      size={18}
+                      strokeWidth={2.5}
+                      className={`transition-transform duration-200 ${isActive ? '' : 'group-hover:scale-110 '}`}
+                    />
+                   {link.text}
+                  </>
+                )}
+              </NavLink>
+            ))}
+          </nav>
+
+          {/* Logout Button */}
+          <div className="mt-auto pt-8">
+            <button
+              onClick={handleLogout}
+              className="w-full flex items-center gap-3 px-4 py-2.5 text-sm font-semibold rounded-xl text-red-600 hover:bg-red-50 transition-all duration-200"
+            >
+              <LogOut size={18} strokeWidth={2.5} />
+              Logout
+            </button>
+          </div>
+        </div>
+      </aside>
+    </>
   )
 }
 
